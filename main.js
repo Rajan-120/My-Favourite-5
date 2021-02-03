aad56b1218b35e601e8e69aa4556840b66da98c1
Rajan-120 <78260275+Rajan-120@users.noreply.github.com>
2021-02-03 23:55:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function showBio() {
+    document.getElementById("bio2").innerHTML = "Sir David Attenborough is Britain's best-known natural history film-maker. His career as a naturalist and broadcaster has spanned nearly seven decades. His first job - after Cambridge University and two years in the Royal Navy - was at a London publishing house. Then in 1952 he joined the BBC as a trainee producer, and it was while working on the Zoo Quest series (1954-64) that he had his first opportunity to undertake expeditions to remote parts of the globe, to capture intimate footage of rare wildlife in its natural habitat. He was Controller of BBC 2 (1965-68), during which time he introduced colour television to Britain, then Director of Programmes for BBC Television (1969-1972). In 1973 he abandoned administration altogether to return to documentary-making and writing, and has established himself as the world's leading Natural History programme maker with severallandmark BBC series, including Life on Earth (1979), The Living Planet (1984), The Trialsof Life (1990), The Private Life of Plants (1995), Life of Birds (1998),The Blue Planet (2001),Life of Mammals (2002), Planet Earth (2006) and Life in Cold Blood (2008). Sir David was knighted in 1985 and received the Order of Merit in 2005. He is a fellow of the Royal Society, and stands at the forefront of issues concerning the planet's declining species and conservation."
+}
+
+function text1() {
+    document.getElementById("david_text").innerHTML = "He is Sir David Attenborough he is famous for natural history film-making. I was inspired by how he loves nature. He never harms nature and he also respects the beauty of nature."
+}
+
+function text2() {
+    document.getElementById("ronaldo_text").innerHTML = "He is Cristiano Ronaldo. I was inspired by his compassionate nature. He sold his gold shoe for 1.4 million euros and the money he got from that he gave all that to Palestinian children."
+}
